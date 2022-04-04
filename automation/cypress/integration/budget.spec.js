5928232aebea3cf926889b185d8c9e9a70eb1200
Rajan Chaudhary <60035342+rajann44@users.noreply.github.com>
2022-04-05 00:02:38 +0530

    cypress support

---
+++ automation/cypress/integration/budget.spec.js
@@ -0,0 +1,37 @@
+describe('Login into App', ()=>{
+
+    it('Perform Signup',()=>{
+        cy.visit("/signup")
+        cy.get('input[type=username]').type('tacekid349')
+        cy.get('input[type=password]').eq(0).type('1')
+        cy.get('input[type=password]').eq(1).type('1')
+        cy.get('button[type=button]').click()
+    //})
+
+
+    //it('Perform Login',()=>{
+        cy.get('input[type=username]').type('tacekid349')
+        cy.get('input[type=password]').type('1')
+        cy.get('button[type=button]').click()
+        cy.url().should('include', 'lists')
+    //})
+
+    //it('Add Budget',()=>{
+        cy.contains('label', 'Budget Name').next('input').type('Budget 1')
+        cy.get('button[type=button]').eq(1).click()
+        cy.contains('h3', 'Budget 1').click()
+    //})
+
+    //it('Add Budget Entry',()=>{
+        cy.contains('label', 'Entry Name').next('input').type('Entry 1')
+        cy.get('select').select('Food')
+        cy.contains('label', 'Amount').next('input').type('2')
+        cy.get('button[type=button]').eq(3).click()
+    //})
+
+    //it('Navigate to stats page',()=>{
+        cy.contains('a', 'Statistics').click()
+        cy.pause()
+    })
+
+})
