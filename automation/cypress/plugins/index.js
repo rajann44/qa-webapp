1dca335245907ecbca9f84b113fa4387df491249
Rajan Chaudhary <60035342+rajann44@users.noreply.github.com>
2022-04-10 15:20:20 +0530

    report fix

--- automation/cypress/plugins/index.js
+++ automation/cypress/plugins/index.js
@@ -17,6 +17,19 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+    require('cypress-mochawesome-reporter/plugin')(on);
 }
+
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
+
+module.exports = (on) => {
+  on('before:run', async (details) => {
+    console.log('override before:run');
+    await beforeRunHook(details);
+  });
+
+  on('after:run', async () => {
+    console.log('override after:run');
+    await afterRunHook();
+  });
+};
